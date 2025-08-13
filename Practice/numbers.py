c23c98aaff6d8b0d24255dd19b405a7472ba2b05
Antonio DFL <antonio.difelice@tiscali.it>
2025-08-13 16:27:09 +0200

    initial commit

---
+++ Practice/numbers.py
@@ -0,0 +1,11 @@
+a=10
+b=0b1010
+c=0o12
+d=0xA
+print(a, b, c, d)
+print(a, bin(b), oct(c), hex(d))
+
+a=0.1
+b=0.2
+sum = a + b
+print("This is the sum:", sum)
---
+++ Practice/strings.py
@@ -0,0 +1,12 @@
+# Multi-line string
+# You need to use triple quotes or triple single quotes
+name = """Antonio 
+é un grande
+"""
+
+print(name)
+# plus is a string concatenator
+name = "Antonio"
+name = name + " e un grande"
+#print(name) 
+print(name*10) 
