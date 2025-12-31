6e34647939e5889dfc28620e7b1536430b962762
Antonio DFL <antonio.difelice@tiscali.it>
2025-12-31 09:11:42 +0100

    changed the path to the relative one in us state game main.py. Forced pyright to ignore a dynamic type with a proper command.

--- Python100daycourse/day-25-us-states-game/main.py
+++ Python100daycourse/day-25-us-states-game/main.py
@@ -1,14 +1,18 @@
 import turtle
 import pandas
+# import os 
+# cwd = os.getcwd() 
+# print("Current Working Directory:", cwd)
+
 screen = turtle.Screen()
 screen.title("US State Game")
-image = "e:\\code\\LearningPython\\Python100daycourse\\day-25-us-states-game\\blank_states_img.gif"
+image = ".\\day-25-us-states-game\\blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 #def get_mouse_click_coor(x,y):
 #    print(x,y)
 #turtle.onscreenclick(get_mouse_click_coor)
-data = pandas.read_csv("e:\\code\\LearningPython\\Python100daycourse\\day-25-us-states-game\\50_states.csv")
+data = pandas.read_csv(".\\day-25-us-states-game\\50_states.csv")
 guessed_states= []
 num_guesses = 0
 all_states = data.state.to_list()
@@ -25,8 +29,8 @@ while len(guessed_states) < 50:
         #     if state not in guessed_states:
         #         states_to_learn.append(state)
         states_to_learn = [state for state in all_states if state not in guessed_states]
-        new_data = pandas.DataFrame(states_to_learn, columns=["State"])
-        new_data.to_csv("e:\\code\\LearningPython\\Python100daycourse\\day-25-us-states-game\\States_to_learn.csv")
+        new_data = pandas.DataFrame(states_to_learn, columns=["State"])  # type: ignore
+        new_data.to_csv(".\\day-25-us-states-game\\States_to_learn.csv")
         break
     if answer_state in all_states:
         state_data = data[data.state == answer_state]
